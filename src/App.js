02d905747fd59ea80915063ae5e53f1cefee3c80
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-06-27 16:21:42 +0100

    Create navBar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 
+import Navbar from './components/navbar';
+
 function App() {
   return (
-    <h1>Test</h1>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => (
+  <header>
+    <div className="logo-section">
+      <span id="logo-text">Space Travelers `&lsquo;`Hub</span>
+      <span>Logo images</span>
+    </div>
+  </header>
+);
+
+export default Navbar;
