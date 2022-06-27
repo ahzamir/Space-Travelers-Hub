2b8542884fa94ba1c3f132997d10e02cf486f5b4
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-06-27 16:44:02 +0100

    Add CSS to the logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logoImg from '../images/logo.png';
 
 const Navbar = () => (
   <header>
     <div className="logo-section">
-      <span id="logo-text">Space Travelers `&lsquo;`Hub</span>
-      <span>Logo images</span>
+      <img className="logo-pic" src={logoImg} alt="logo" />
+      <span id="logo-text">Space Travelers &lsquo; Hub</span>
     </div>
   </header>
 );
