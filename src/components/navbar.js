3a3011a5a02bec431ffc1f2bb267829db82836da
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-27 20:56:12 +0430

    added the link path for 3 pages

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,14 +3,31 @@ import logoImg from '../images/logo.png';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: '/missions',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      path: '/myprofile',
+      text: 'My Profile',
+    },
+  ];
   return (
-  <header>
-    <div className="logo-section">
-      <img className="logo-pic" src={logoImg} alt="logo" />
-      <span id="logo-text">Space Travelers &lsquo; Hub</span>
-    </div>
-  </header>
-)};
+    <header>
+      <div className="logo-section">
+        <img className="logo-pic" src={logoImg} alt="logo" />
+        <span id="logo-text">Space Travelers &lsquo; Hub</span>
+      </div>
+    </header>
+  )
+};
 
 export default Navbar;
