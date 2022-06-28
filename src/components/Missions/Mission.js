f92f242eba6bdd2e7552b7d579a4b563801da2d0
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-28 15:26:22 +0430

    add the code for managing the store missions and add them on the mission page

---
+++ src/components/Missions/Mission.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Mission = ({ title, description }) => (
+  <li className='missions border'>
+    <h2 className='border missionContents'>
+      {title}
+    </h2>
+    <p className='border missionContents missionDescription'>
+      {description}
+    </p>
+    <p className='border missionContents'>
+      Not a member
+    </p>
+    <div className='border missionContents missionButtons'>
+      <button
+        type="button"
+        className='missionButton'
+      >
+        Join Mission
+      </button>
+    </div>
+  </li>
+);
+
+export default Mission;
--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../redux-states/missions/MissionState';
+import Mission from './Mission';
 
-const Missions = () => (
-  <div>
-    this is miisions page
-  </div>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions);
+  useEffect(() => {
+    dispatch(getMissions());
+  }, []);
+  return (
+    <ul className='missionsLists'>
+      <li className='missions border'>
+        <h2 className='border missionContents'>Missions</h2>
+        <h2 className='border missionContents'>Description</h2>
+        <h2 className='border missionContents'>Status</h2>
+        <p className='border missionContents'></p>
+      </li>
+      {missions.map((mission) => (
+        <Mission
+          key={mission.mission_id}
+          title={mission.mission_name}
+          description={mission.description}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default Missions;
