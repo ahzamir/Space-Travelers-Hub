a3709682050790342fceb2cf9ad29c23eb6c02a8
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-06-28 17:40:44 +0100

    Add the booking rocket action

--- src/redux-states/rockets/RocketState.js
+++ src/redux-states/rockets/RocketState.js
@@ -1,13 +1,22 @@
+import axios from 'axios';
+
 const GET_ROCKETS = 'Space-Travelers-Hub/rockets/GET_ROCKETS';
+const BOOKING_ROCKET = 'Space-Travelers-Hub/rockets/BOOKING_ROCKET';
+
 export const getRockets = (payload) => ({
   type: GET_ROCKETS,
   payload,
 });
 
+export const bookingRocket = (payload) => ({
+  type: BOOKING_ROCKET,
+  payload,
+});
+
 const url = 'https://api.spacexdata.com/v3/rockets';
 export const fetchRockets = () => async (dispatch) => {
-  const response = await fetch(url);
-  const results = await response.json();
+  const response = await axios.get(url);
+  const results = await response.data;
   const rockets = [];
   Object.entries(results).forEach((result) => {
     rockets.push({ ...result[1] });
@@ -19,6 +28,11 @@ const rocketReducer = (state = [], action) => {
   switch (action.type) {
     case GET_ROCKETS:
       return action.payload;
+    case BOOKING_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.id !== action.payload) return rocket;
+        return { ...rocket, reserved: !rocket.reserved };
+      });
     default:
       return state;
   }
