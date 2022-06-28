e70ecb14e6c5b96267db91ccea76a239dc451081
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-28 15:17:15 +0430

    get the missions from API

--- src/redux-states/missions/MissionState.js
+++ src/redux-states/missions/MissionState.js
@@ -1,3 +1,13 @@
 const GET_MISSIONS = 'my-app/missions/GET_MISSIONS';
 const initialState = [];
 
+const apiGetMissions = async () => {
+  const response = await fetch('https://api.spacexdata.com/v3/missions', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+  return data;
+};
