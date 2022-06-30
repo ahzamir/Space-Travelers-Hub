f1b6f774db5980bb3c476b15be8375eb3abcdf12
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-30 13:28:03 +0430

    testing the mission component for success  render the element on the dom

--- src/components/Missions/__test__/Mission.test.js
+++ src/components/Missions/__test__/Mission.test.js
@@ -5,26 +5,14 @@ import renderer from "react-test-renderer";
 import { Provider } from "react-redux";
 import store from "../../../redux-states/configureStore";
 
-import Missions from '../Missions';
+import Mission from '../Mission';
 
 describe('Testing Mission component', () => {
   render(<Provider store={store}>
-    <Missions />
+    <Mission />
   </Provider>);
   test('Testing the dom for success rendering elements on Mission component', () => {
-    expect(screen.queryByTestId('missionsContent')).toBeInTheDocument();
+    expect(screen.queryByTestId('missionContent')).toBeInTheDocument();
   });
 
-  test('Testing the dom for non existing elemente on the page after rendering', () => {
-    expect(screen.queryByTestId('nothing')).not.toBeInTheDocument();
-  });
-
-  it('renders correctly', () => {
-    const tree = renderer
-      .create(<Provider store={store}>
-        <Missions page="just for test">Calculator</Missions>
-      </Provider>)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
 })
