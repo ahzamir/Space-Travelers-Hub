c4353f5245300709d893bbdf3f9f51a29c71cc80
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-06-30 14:27:32 +0100

    Fix navbar error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getMissions } from './redux-states/missions/MissionState';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import Rockets from './components/rockets/Rockets';
 import Missions from './components/Missions/Missions';
 import MyProfile from './components/MyProfile';
--- src/components/__test__/navbar.test.js
+++ src/components/__test__/navbar.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from '../Navbar';
+import Navbar from '../navbar';
 
 it('renders correctly', () => {
   const navbar = renderer
